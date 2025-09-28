304a7fad07434e5c49880b7ddb5bdead8438ba14
RegeleArdealului <152423334+Regeleardealului@users.noreply.github.com>
2025-09-28 23:54:20 +0200

    Update tool.py

--- tool.py
+++ tool.py
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 # Get the correct file path for Streamlit Cloud
 #current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 #model_path = current_dir / "best_vgg_enhanced.keras"
-model_path = "best_vgg_enhanced.keras"
+model_path = "./best_vgg_enhanced.keras"
 
 # Load model with error handling
 try:
